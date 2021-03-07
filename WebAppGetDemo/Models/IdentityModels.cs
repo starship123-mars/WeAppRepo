7be425abc506cc0f5365cc41ab81409994d5307e
mittal <mittal@MITTAL-PC>
2021-03-07 10:55:59 +0530

    add employee Controller

--- WebAppGetDemo/Models/IdentityModels.cs
+++ WebAppGetDemo/Models/IdentityModels.cs
@@ -25,6 +25,8 @@ namespace WebAppGetDemo.Models
         {
         }
 
+        public DbSet<Employee> Employees { get; set; }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
